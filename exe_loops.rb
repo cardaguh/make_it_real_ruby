671cc407355f3b437f91a509ad5442a8b2c0b0fd
Carlos Daniel Agudelo Henao <carlosagudelo@Carloss-MacBook-Air.local>
2017-10-11 21:08:02 -0500

    otra con ejercicios

---
+++ exe_loops.rb
@@ -0,0 +1,29 @@
+# Usuario ingresa un número, generar un numero aleatorio entre x y y,
+# decirle al usuario si el número que ingresó está por debajo, por encima
+# o acertó
+# Contar repeticiones
+#
+
+count = 0
+found = false
+random_num = rand(1...10)
+i = 0
+#puts random_num
+
+while found != true
+  print "Adivina el número: "
+  num = gets.chomp.to_i
+
+  if num == random_num
+    puts "Adivinaste! Felicitaciones!"
+    found = true
+  elsif num > random_num
+    puts "Debajo"
+  elsif num < random_num
+    puts "Encima"
+  end
+  count += 1
+  i += 1
+end
+
+puts "Se repitió #{count}"
